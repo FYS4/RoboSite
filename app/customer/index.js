c56cd5e6aeb3a21c6b10db1e14d89e841d4ecedb
Martijn Vegter <martijn@martijnvegter.com>
2017-12-06 12:42:33 +0100

    WIP Customer Routing

--- app/customer/index.js
+++ app/customer/index.js
@@ -1,36 +1,25 @@
-const express = require('express');
-const path = require('path');
+'use strict';
 
-const index = require('./routes/index');
-const users = require('./routes/users');
+/* Core */
+const express = require('express'); // Fast, unopinionated, minimalist web framework for Node.js
+const path = require('path'); // Provides utilities for working with file and directory paths
+
+/* App */
+const routes = require('./routes');
 
 module.exports = (app) => {
-	// view engine setup
+	/* view engine setup */
 	app.set('views', path.join(__dirname, 'views'));
 	app.set('view engine', 'hbs');
 
-	app.use(express.static(path.join(__dirname, 'public')));
+	/* Cache public folder */
+	app.use(express.static(path.resolve(__dirname, 'public'), {
+		maxage: 1 * 60 * 1000
+	}));
 
-	app.use('/', index);
-	app.use('/users', users);
+	/* Apply routes to object */
+	app = routes(app);
 
-	// catch 404 and forward to error handler
-	app.use(function(req, res, next) {
-		const err = new Error('Not Found');
-		err.status = 404;
-		next(err);
-	});
-
-	// error handler
-	app.use(function(err, req, res, next) {
-		// set locals, only providing error in development
-		res.locals.message = err.message;
-		res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-		// render the error page
-		res.status(err.status || 500);
-		res.render('error');
-	});
-
+	/* Return updated object */
 	return app;
 };
