35cdcedd96e4c7e8887d09d758059894b47862b0
Martijn Vegter <mail@mvegter.me>
2017-12-06 12:15:57 +0100

    Added ESLint Support

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "comma-dangle": [
+          "error",
+          "never"
+        ],
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "no-multiple-empty-lines": [
+          "error",
+          {
+            "max": 1,
+            "maxBOF": 1
+          }
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-var": "error"
+    }
+};
