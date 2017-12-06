126dc0b6ed0b5992d17b71fdad62f08125d33695
Martijn Vegter <mail@mvegter.me>
2017-12-06 14:59:48 +0100

    WIP Login / Vergeten / Reset

--- app/customer/routes/home.js
+++ app/customer/routes/home.js
@@ -4,11 +4,32 @@
 const express = require('express'); // Fast, unopinionated, minimalist web framework for Node.js
 const router = express.Router();
 
+const config = require('../../config').Authentication;
+
 /* GET users listing. */
 router.get('/', (req, res) => {
-	res.render('home', {
-		user: true
-	});
+  res.render('home', {
+    user: true
+  });
+});
+
+/* GET users listing. */
+router.get('/login', (req, res) => {
+  res.render('login', {
+    showLocal: config.Local.Enabled,
+    showFacebook: config.Facebook.Enabled,
+    showGoogle: config.Google.Enabled,
+  });
+});
+
+/* GET users listing. */
+router.get('/vergeten', (req, res) => {
+  res.render('forgot', {});
+});
+
+/* GET users listing. */
+router.get('/reset', (req, res) => {
+  res.render('reset', {});
 });
 
 module.exports = router;
